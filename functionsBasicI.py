48aad6bbfd6db89699895760e25a78ae5a479dbc
qidCoder <qidcoder@gmail.com>
2020-09-29 11:44:15 -0400

    Included 11-14

--- functionsBasicI.py
+++ functionsBasicI.py
@@ -88,3 +88,66 @@ def a(b,c):
 print(a(3,5))
 #prediction
 #8
+
+#11
+b = 500
+print(b)
+def a():
+    b = 300
+    print(b)
+print(b)
+a()
+print(b)
+#prediction
+#500, 500, 300, 500
+
+#12
+b = 500
+print(b)
+def a():
+    b = 300
+    print(b)
+    return b
+print(b)
+a()
+print(b)
+#prediction
+#500, 500, 300, 500
+
+#13
+b = 500
+print(b)
+def a():
+    b = 300
+    print(b)
+    return b
+print(b)
+b=a()
+print(b)
+#prediction
+#500, 500, 300, 300
+
+#14
+def a():
+    print(1)
+    b()
+    print(2)
+def b():
+    print(3)
+a()
+#prediction
+#1, 3, 2
+
+#15
+def a():
+    print(1)
+    x = b()
+    print(x)
+    return 10
+def b():
+    print(3)
+    return 5
+y = a()
+print(y)
+#prediction
+#1, 3, 5, 10
